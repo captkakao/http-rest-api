26f25ae13d7c194fbfa31a1206764dea839170dc
Sultanbek <soultouchka@gmail.com>
2022-08-23 23:51:27 +0600

    ch3

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,6 +1,10 @@
 package model
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID                int
@@ -20,6 +24,14 @@ func (u *User) BeforeCreate() error {
 	}
 
 	return nil
+}
+
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required, validation.Length(6, 100)),
+	)
 }
 
 func encryptString(s string) (string, error) {
